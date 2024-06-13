b1964191e7b795ddf3a094b3e4a7bf74520e4091
song <usnara9465@naver.com>
2024-06-13 15:21:32 +0900

    6.13

--- fast-food/src/main/java/com/boot/fastfood/controller/MainController.java
+++ fast-food/src/main/java/com/boot/fastfood/controller/MainController.java
@@ -5,8 +5,52 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-    @GetMapping("/main")
+    @GetMapping("/item")
     public String mainPage(){
-        return "blank";
+        return "item";
     }
+    @GetMapping("/item1")
+    public String maPage(){
+        return "item1";
+    }
+
+    @GetMapping("/process")
+    public String rocess(){
+        return "process";
+    }
+    @GetMapping("/routing")
+    public String routingP(){
+        return "routing";
+    }
+
+    @GetMapping("/routingAdd")
+    public String routingAddP(){
+        return "routingAdd";
+    }
+
+    @GetMapping("/bom")
+    public String bomP(){
+        return "bom";
+    }
+
+    @GetMapping("/bom1")
+    public String bP(){
+        return "bom1";
+    }
+
+    @GetMapping("/code")
+    public String codeP(){
+        return "code";
+    }
+
+    @GetMapping("/vendor")
+    public String vendorP(){
+        return "vendor";
+    }
+
+    @GetMapping("/client")
+    public String clientP(){
+        return "client";
+    }
+
 }
