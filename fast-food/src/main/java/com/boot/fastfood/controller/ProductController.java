a3d52a2c912c1cec611f4ce3ad8acc04003b7359
ramperrit <153475173+ramperrit@users.noreply.github.com>
2024-06-14 17:59:42 +0900

    .

--- fast-food/src/main/java/com/boot/fastfood/controller/ProductController.java
+++ fast-food/src/main/java/com/boot/fastfood/controller/ProductController.java
@@ -1,14 +1,22 @@
 package com.boot.fastfood.controller;
 
+import com.boot.fastfood.entity.Contract;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/machine")
 public class ProductController {
+//    private final ContractService contractService;
+
+
     @GetMapping("/productPlan")
-    public String productPlan(){
+    public String productPlan(Model model){
+//        List<Contract> contracts = contractService.getAllContracts();
         return "ProductPages/ProductionPlan";
     }
 
