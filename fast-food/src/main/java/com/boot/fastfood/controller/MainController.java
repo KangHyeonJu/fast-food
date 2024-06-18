3d37643bba5791ba724071723b58fd01c264df79
KangHyeonJu <90234861+KangHyeonJu@users.noreply.github.com>
2024-06-18 18:59:27 +0900

    .

--- fast-food/src/main/java/com/boot/fastfood/controller/MainController.java
+++ fast-food/src/main/java/com/boot/fastfood/controller/MainController.java
@@ -8,44 +8,5 @@ public class MainController {
     @GetMapping("/main")
     public String mainPage(){return "main/main";}
 
-    @GetMapping("/process")
-    public String processPage(){
-        return "system/process";
-    }
-    @GetMapping("/add")
-    public String mae(){
-        return "system/add";
-    }
-
-    @GetMapping("/process1")
-    public String rocess(){
-        return "processs";
-    }
-    @GetMapping("/routing1")
-    public String routingP(){
-        return "routingg";
-    }
-
-    @GetMapping("/routingAdd")
-    public String routingAddP(){
-        return "routingAdd";
-    }
-
-    @GetMapping("/code")
-    public String codeP(){
-        return "system/code";
-    }
-
-    @GetMapping("/client")
-    public String clientP(){
-        return "system/client";
-    }
-
-    @GetMapping("/name")
-    public String cntP(){
-        return "system/name";
-    }
-
-
 }
 
