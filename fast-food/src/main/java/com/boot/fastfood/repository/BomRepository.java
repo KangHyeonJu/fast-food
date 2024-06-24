780a3abf3128c366721f230f1e845716ff9845e3
KangHyeonJu <90234861+KangHyeonJu@users.noreply.github.com>
2024-06-24 12:28:46 +0900

    .

--- fast-food/src/main/java/com/boot/fastfood/repository/BomRepository.java
+++ fast-food/src/main/java/com/boot/fastfood/repository/BomRepository.java
@@ -1,16 +1,13 @@
 package com.boot.fastfood.repository;
 
 import com.boot.fastfood.entity.BOM;
+import com.boot.fastfood.entity.BOMId;
 import com.boot.fastfood.entity.Items;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface BomRepository extends JpaRepository<BOM, String> {
-    List<BOM> findByItems(Items items);
+public interface BOMRepository extends JpaRepository<BOM, String> {
 
-    List<BOM> findByItems_ItCode(String itCode);
-
-    @Query("SELECT r FROM BOM r WHERE r.items.itCode = :itCode AND r.materials.mtCode = :mtCode")
-    BOM findByItCodeAndMtCode(@Param("itCode") String itCode, @Param("mtCode") String mtCode);
+    List<BOM> findByItems(Items itCode);
 }
