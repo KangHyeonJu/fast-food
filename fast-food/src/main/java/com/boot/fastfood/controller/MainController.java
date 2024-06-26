1f26bbd7cd5f3951bd827a5d00462ba10eeacbaf
yangminheouk <154565474+yangminheouk@users.noreply.github.com>
2024-06-26 13:51:36 +0900

    ㅇㅇ
    ㅇ

--- fast-food/src/main/java/com/boot/fastfood/controller/MainController.java
+++ fast-food/src/main/java/com/boot/fastfood/controller/MainController.java
@@ -1,6 +1,8 @@
 package com.boot.fastfood.controller;
 
+import com.boot.fastfood.entity.Calendar;
 import com.boot.fastfood.entity.Facility;
+import com.boot.fastfood.service.CalendarService;
 import com.boot.fastfood.service.FacilityService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -13,11 +15,17 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MainController {
     private final FacilityService facilityService;
+    private final CalendarService calendarService;
+
     @GetMapping("/main")
     public String mainPage(Model model){
         List<Facility> facilities = facilityService.getAllFacility();
 
         model.addAttribute("facilities", facilities);
+
+        List<Calendar> calendars = calendarService.getAllEvents();
+
+        model.addAttribute("calendars", calendars);
 
         return "main/main";
     }
